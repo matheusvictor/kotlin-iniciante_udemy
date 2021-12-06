6800a615e08394c82275a01f46830583c414e8e0
matheusvictor <matheusvictor.salles@gmail.com>
2021-12-06 15:22:05 -0300

    Implementa funcionalidade para calcular gasto

--- Android_app/app/src/main/java/com/example/gastodeviagem/MainActivity.kt
+++ Android_app/app/src/main/java/com/example/gastodeviagem/MainActivity.kt
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val botaoCalcular = findViewById<Button>(R.id.botaoCalcular)
         botaoCalcular.setOnClickListener(this)
 
     }
@@ -22,5 +24,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    private fun calcularGasto() {}
+    private fun calcularGasto() {
+        val preco = inputPreco.text.toString().toFloat()
+        val distancia = inputDistancia.text.toString().toFloat()
+        val autonomia = outputAutonomia.text.toString().toFloat()
+
+        val valorTotalGasto = (distancia * preco) / autonomia
+        textoValorTotal.text = "R$ ${"%.2f".format(valorTotalGasto)}"
+    }
 }
