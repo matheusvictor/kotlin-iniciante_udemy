67982af8dbc2c0371ae3fbed211c386a7d494b27
matheusvictor <matheusvictor.salles@gmail.com>
2021-12-06 14:19:53 -0300

    Adiciona evento ao clicar no botão

--- Android_app/app/src/main/java/com/example/gastodeviagem/MainActivity.kt
+++ Android_app/app/src/main/java/com/example/gastodeviagem/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.gastodeviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botaoCalcular = findViewById<Button>(R.id.botaoCalcular)
+        botaoCalcular.setOnClickListener(this)
+
     }
+
+    override fun onClick(view: View) {
+        val id = view.id
+        if (id == R.id.botaoCalcular) {
+            calcularGasto()
+        }
+    }
+
+    private fun calcularGasto() {}
 }
