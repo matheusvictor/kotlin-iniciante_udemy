1acb03dad66b3962fbb6b24ce0923743660385ec
matheusvictor <matheusvictor.salles@gmail.com>
2021-12-07 09:38:11 -0300

    Implementa alertas de validação para preenchimento correto dos campos

--- Android_app/app/src/main/java/com/example/gastodeviagem/MainActivity.kt
+++ Android_app/app/src/main/java/com/example/gastodeviagem/MainActivity.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,13 +28,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun calcularGasto() {
         if (validarCampos()) {
-            val preco = inputPreco.text.toString().toFloat()
-            val distancia = inputDistancia.text.toString().toFloat()
-            val autonomia = inputAutonomia.text.toString().toFloat()
 
-            val valorTotalGasto = (distancia * preco) / autonomia
-            textoValorTotal.text = "R$ ${"%.2f".format(valorTotalGasto)}"
-        }
+            if (inputAutonomia.text.toString() == "0") {
+                Toast.makeText(
+                    this, getString(R.string.aviso_autonomia_diferente_zero),
+                    Toast.LENGTH_LONG
+                ).show()
+                inputAutonomia.text.clear()
+            } else {
+                try {
+                    val preco = inputPreco.text.toString().toFloat()
+                    val distancia = inputDistancia.text.toString().toFloat()
+                    val autonomia = inputAutonomia.text.toString().toFloat()
+
+                    val valorTotalGasto = (distancia * preco) / autonomia
+                    textoValorTotal.text = "R$ ${"%.2f".format(valorTotalGasto)}"
+
+                } catch (erroFormatoNumero: NumberFormatException) {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.informar_valores_validos),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        } else Toast.makeText(this, getString(R.string.preencha_todos_campos), Toast.LENGTH_LONG)
+            .show()
     }
 
     private fun validarCampos(): Boolean {
