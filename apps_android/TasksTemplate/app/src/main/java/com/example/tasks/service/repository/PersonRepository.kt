74c6777b6fbb3a1ed7a45b6450f2a98b39352c79
Matheus Victor <matheusvictor.salles@gmail.com>
2022-01-18 20:49:46 -0300

    Implementa interface para escutar os retornos das chamadas da API

--- apps_android/TasksTemplate/app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
+++ apps_android/TasksTemplate/app/src/main/java/com/example/tasks/service/repository/PersonRepository.kt
@@ -1,14 +1,38 @@
 package com.example.tasks.service.repository
 
+import com.example.tasks.service.listener.APIListener
+import com.example.tasks.service.model.HeaderModel
 import com.example.tasks.service.repository.remote.PersonService
 import com.example.tasks.service.repository.remote.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class PersonRepository {
 
     private val mRetrofitInstance = RetrofitClient.createService(PersonService::class.java)
 
-    fun login(email: String, password: String) {
-        //TODO("ainda será implementado")
+    fun login(email: String, password: String, listener: APIListener) {
+        val call: Call<HeaderModel> = mRetrofitInstance.login(email, password)
+        // Realiza chamada assíncrona:
+        call.enqueue(
+            object : Callback<HeaderModel> {
+
+                /*
+                A falha só ocorre se a requisição para a API não obteve sucesso.
+                O método onFailure será será chamado se a requisição não for feita com sucesso, do
+                contrário, procederá normalmente para onResponse - podendo variar apenas o código
+                de retorno da requisição.
+                 */
+                override fun onFailure(call: Call<HeaderModel>, t: Throwable) {
+                    listener.onFailure(t.message.toString())
+                }
+
+                override fun onResponse(call: Call<HeaderModel>, response: Response<HeaderModel>) {
+                    response.body()?.let { listener.onSuccess(it) }
+                }
+            }
+        )
     }
 
 }
--- apps_android/TasksTemplate/app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
+++ apps_android/TasksTemplate/app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -2,13 +2,29 @@ package com.example.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.example.tasks.service.listener.APIListener
+import com.example.tasks.service.model.HeaderModel
+import com.example.tasks.service.repository.PersonRepository
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val mPersonRepository = PersonRepository()
 
     /**
      * Faz login usando API
      */
     fun doLogin(email: String, password: String) {
+        mPersonRepository.login(email, password, object : APIListener {
+
+            override fun onSuccess(model: HeaderModel) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFailure(message: String) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 
     /**
