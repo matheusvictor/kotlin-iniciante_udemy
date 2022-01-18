9d24c1789144948fa09dcb7da1b829d3c200326f
Matheus Victor <matheusvictor.salles@gmail.com>
2022-01-18 20:57:38 -0300

    Implementa variável observável para login

--- apps_android/TasksTemplate/app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
+++ apps_android/TasksTemplate/app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -2,6 +2,8 @@ package com.example.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.model.HeaderModel
 import com.example.tasks.service.repository.PersonRepository
@@ -10,6 +12,9 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     private val mPersonRepository = PersonRepository()
 
+    private val mLoginSuccessed = MutableLiveData<Boolean>()
+    var loginSuccessed: LiveData<Boolean> = mLoginSuccessed
+
     /**
      * Faz login usando API
      */
@@ -17,13 +22,12 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
         mPersonRepository.login(email, password, object : APIListener {
 
             override fun onSuccess(model: HeaderModel) {
-                TODO("Not yet implemented")
+                mLoginSuccessed.value = true
             }
 
             override fun onFailure(message: String) {
-                TODO("Not yet implemented")
+                mLoginSuccessed.value = true
             }
-
         })
     }
 
