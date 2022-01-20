e7fb39483364041688a754466b991486b47cac50
Matheus Victor <matheusvictor.salles@gmail.com>
2022-01-20 11:02:13 -0300

    Add DatePicker
    Seta o listener para evento do botão de seleção de data e adiciona método para exibir diálogo com o usuário. A data selecionada é formatada para String no formato dd/MM/yyyy

--- apps_android/TasksTemplate/app/src/main/java/com/example/tasks/view/TaskFormActivity.kt
+++ apps_android/TasksTemplate/app/src/main/java/com/example/tasks/view/TaskFormActivity.kt
@@ -1,16 +1,27 @@
 package com.example.tasks.view
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.DatePicker
 import androidx.lifecycle.ViewModelProvider
 import com.example.tasks.R
 import com.example.tasks.viewmodel.RegisterViewModel
 import kotlinx.android.synthetic.main.activity_register.*
+import kotlinx.android.synthetic.main.activity_register.button_save
+import kotlinx.android.synthetic.main.activity_task_form.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
-class TaskFormActivity : AppCompatActivity(), View.OnClickListener {
+class TaskFormActivity : AppCompatActivity(),
+    View.OnClickListener,
+    DatePickerDialog.OnDateSetListener {
 
     private lateinit var mViewModel: RegisterViewModel
+
+    private val mDateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +43,8 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener {
             val password = edit_password.text.toString()
 
             mViewModel.create(name, email, password)
+        } else if (id == R.id.button_date) {
+            showDatePicker()
         }
     }
 
@@ -40,6 +53,26 @@ class TaskFormActivity : AppCompatActivity(), View.OnClickListener {
 
     private fun listeners() {
         button_save.setOnClickListener(this)
+        button_date.setOnClickListener(this)
+    }
+
+    private fun showDatePicker() {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this, this, year, month, day).show()
+    }
+
+    // listener chamado quando o usuário selecionou a data desejada
+    override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
+        val calendar = Calendar.getInstance()
+        calendar.set(year, month, day)
+
+        //formatação de data
+        val dateInTextForm = mDateFormat.format(calendar.time)
+        button_date.text = dateInTextForm
+
     }
 
 }
