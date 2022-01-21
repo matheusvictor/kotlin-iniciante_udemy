4e4e979c6f2f7bcda6a72a5e1356cb51348a991f
Matheus Victor <matheusvictor.salles@gmail.com>
2022-01-21 09:21:05 -0300

    Adiciona filtros para listagem das tarefas

--- apps_android/TasksTemplate/app/src/main/java/com/example/tasks/viewmodel/AllTasksViewModel.kt
+++ apps_android/TasksTemplate/app/src/main/java/com/example/tasks/viewmodel/AllTasksViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.tasks.service.constants.TaskConstants
 import com.example.tasks.service.listener.APIListener
 import com.example.tasks.service.listener.ValidationListener
 import com.example.tasks.service.model.TaskModel
@@ -12,6 +13,7 @@ import com.example.tasks.service.repository.TaskRepository
 class AllTasksViewModel(application: Application) : AndroidViewModel(application) {
 
     private val mTaskRepository = TaskRepository(application)
+    private var mTaskFilter = 0
 
     private val mValidation = MutableLiveData<ValidationListener>()
     var validation: LiveData<ValidationListener> = mValidation
@@ -19,22 +21,33 @@ class AllTasksViewModel(application: Application) : AndroidViewModel(application
     private val mTaskList = MutableLiveData<List<TaskModel>>()
     var tasks: LiveData<List<TaskModel>> = mTaskList
 
-    fun listAllTasks() {
-        mTaskRepository.allTasks(object : APIListener<List<TaskModel>> {
+    fun listAllTasks(taskFilter: Int) {
+        mTaskFilter = taskFilter
+
+        val listener = object : APIListener<List<TaskModel>> {
             override fun onSuccess(model: List<TaskModel>) {
                 mTaskList.value = model
             }
+
             override fun onFailure(message: String) {
                 mTaskList.value = arrayListOf()
                 mValidation.value = ValidationListener(message)
             }
-        })
+        }
+
+        if (mTaskFilter == TaskConstants.FILTER.ALL) {
+            mTaskRepository.allTasks(listener)
+        } else if (mTaskFilter == TaskConstants.FILTER.NEXT) {
+            mTaskRepository.nextWeek(listener)
+        } else {
+            mTaskRepository.overDue(listener)
+        }
     }
 
     fun delete(id: Int) {
         mTaskRepository.deleteTask(id, object : APIListener<Boolean> {
             override fun onSuccess(model: Boolean) {
-                listAllTasks()
+                listAllTasks(mTaskFilter)
                 mValidation.value = ValidationListener()
             }
 
@@ -55,7 +68,7 @@ class AllTasksViewModel(application: Application) : AndroidViewModel(application
     private fun updateStatus(id: Int, complete: Boolean) {
         mTaskRepository.updateStatus(id, complete, object : APIListener<Boolean> {
             override fun onSuccess(model: Boolean) {
-                listAllTasks()
+                listAllTasks(mTaskFilter)
             }
 
             override fun onFailure(message: String) {
